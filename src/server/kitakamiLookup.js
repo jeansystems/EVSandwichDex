041e0654a970776b39db99c1592afb5c36f52535
jeansystems <trjean.ou@gmail.com>
2023-10-27 23:51:08 -0500

    almost complete API data search

--- src/server/kitakamiLookup.js
+++ src/server/kitakamiLookup.js
@@ -1,5 +1,9 @@
+//todo: create try/catch blocks for API requests
+
 const kitakamiPokedex = "https://pokeapi.co/api/v2/pokedex/32"
 
+//variable naming is tedious, should consider alternatives
+//for now, we pull the URL of each Pokemon "species", covering variants for Pokemon with regional variants or other forms
 async function fetchKitakamiSpeciesUrls() {
 
 	let kitakamiSpeciesUrls = [];
@@ -16,37 +20,74 @@ async function fetchKitakamiSpeciesUrls() {
 
 let kitakamiSpeciesUrls = await fetchKitakamiSpeciesUrls();
 
-
-//https://www.samjarman.co.nz/blog/promisedotall
-//Further reading on new concept to me, Promises.all()
-async function fetchKitakamiPokemonUrls(kitakamiSpeciesUrls) {
-    const responses = await Promise.all(kitakamiSpeciesUrls.map (async (species) => {
-        const response = await fetch(species);
-        const result = await response.json();
-        console.log(result.varieties);
-    }));
-}
-
-let kitakamiPokemon = await fetchKitakamiPokemonUrls(kitakamiSpeciesUrls);
-
-/*
-async function fetchKitakamiPokemonStats(kitakamiPokemon) {
-    const urls = await Promise.all(kitakamiPokemon.map (async (species) => {
-        const url = await
-    }))
-}
-
-/*
-//keeping old function below
+//now that we have each "species", we can retrieve the URL for each Pokemon
+//the URLs requested here contain the actual data we're going for: EV yields
 async function fetchKitakamiPokemonUrls(kitakamiSpeciesUrls) {
 	
 	let kitakamiPokemonUrls = [];
-
-	for(let species of kitakamiSpeciesUrls) {
+	
+	const responses = await Promise.all(kitakamiSpeciesUrls.map (async (species) => {
 		const response = await fetch(species);
-		const result = await response.json()
+		const result = await response.json();
+		return result.varieties.forEach((pokemon_species) =>
+			kitakamiPokemonUrls.push(pokemon_species.pokemon.url)
+	)}));
+	return kitakamiPokemonUrls;
+}
 
-		kitakamiPokemonUrls.push(result.name);
-        console.log(result.name);
-	};
-}*/
+let kitakamiPokemon = await fetchKitakamiPokemonUrls(kitakamiSpeciesUrls);
+let kitakamiPokemonSlice = kitakamiPokemon.slice(0,3);
+
+async function fetchKitakamiStats(kitakamiPokemonSlice) {
+
+	let kitakamiStats = [];
+
+	const responses = await Promise.all(kitakamiPokemonSlice.map (async (pokemon_data) => {
+		const response = await fetch(pokemon_data);
+		const result = await response.json();
+		
+		const name = result.name;
+		const stats = result.stats;
+		kitakamiStats.push({ name, stats });
+	}));
+	return kitakamiStats;
+}
+
+let finalStats = await fetchKitakamiStats(kitakamiPokemonSlice);
+console.log(finalStats, typeof finalStats, finalStats[0].stats);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+async function fetchKitakamiStats(kitakamiPokemonSlice) {
+
+	let kitakamiStats = [];
+
+	const responses = await Promise.all(kitakamiPokemonSlice.map (async (pokemonUrl) => {
+		const response = await fetch(pokemonUrl);
+		const result = await response.json();
+		return result.forEach((pokemonStats) =>
+			kitakamiStats.push(pokemonStats.stats)
+	)}));
+	return kitakamiStats;
+}
+
+let finalStats = await fetchKitakamiStats(kitakamiSpeciesUrls);
+*/
