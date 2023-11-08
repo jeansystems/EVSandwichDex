2c81cfabdfea99807485694ebc4d07f5dcfb64c2
jeansystems <trjean.ou@gmail.com>
2023-11-08 15:35:47 -0600

    data manips for eventual JSON creation

--- server_py/kitakami_lookup.py
+++ server_py/kitakami_lookup.py
@@ -15,12 +15,12 @@ def get_species_urls(POKEDEX):
 async def get_pokemon_datasets(species_url):
     async with httpx.AsyncClient() as client:
         resp = await client.get(species_url)
-        print(f"Gathering data for {species_url}")
+        #print(f"Gathering data for {species_url}")
         return resp.json()
 
 async def get_varieties_from_datasets(species_urls):
-    #tasks = [asyncio.create_task(get_pokemon_datasets(species_url)) for species_url in species_urls]
-    tasks = [asyncio.create_task(get_pokemon_datasets(species_url)) for species_url in species_urls[0:3]]
+    tasks = [asyncio.create_task(get_pokemon_datasets(species_url)) for species_url in species_urls]
+    #tasks = [asyncio.create_task(get_pokemon_datasets(species_url)) for species_url in species_urls[0:3]]
     for coro in asyncio.as_completed(tasks):
         result = await coro
         if result:
@@ -32,7 +32,7 @@ async def get_varieties_stats(variety_url):
     pokemon_url = variety_url["pokemon"]["url"]
     async with httpx.AsyncClient() as client:
         resp = await client.get(pokemon_url)
-        print(f"Data for {pokemon_url}")
+        #print(f"Data for {pokemon_url}")
         return resp.json()
 
 
@@ -40,10 +40,16 @@ async def parse_varieties_urls(varieties_urls):
     tasks = [asyncio.create_task(get_varieties_stats(variety_url)) for variety_url in varieties_urls]
     results = await asyncio.gather(*tasks)
     #finally we can build objects with the correct data from proper pokemon entries
+    #here we can tinker with data to decide what to build our own server-side JSON with
     for result in results:
         stats = result["stats"]
-        name = result["name"].capitalize()
-        print(f"Pokemon {name} has the following stats block:\n{stats}")
+        name = result["name"]
+        order = result["order"]
+        isdefault = result["is_default"]
+        #if isdefault == True:
+        #    print(f"Default: True  \t{name}")
+        if isdefault == False:
+            print(f"Default: False \t{name}")
 
 POKEDEX = 'https://pokeapi.co/api/v2/pokedex/32'
 species_urls = get_species_urls(POKEDEX)
