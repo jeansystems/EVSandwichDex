e3029b8d7ea099ad62354110112992667aaa9326
jeansystems <trjean.ou@gmail.com>
2023-12-05 16:22:37 -0600

    dicts to JSON trouble

--- server_py/kitakami_lookup.py
+++ server_py/kitakami_lookup.py
@@ -10,7 +10,7 @@ import json
 def get_species_urls(POKEDEX):
     resp = httpx.get(POKEDEX)
     result = resp.json()
-    species_urls = [entry["pokemon_species"]["url"] for entry in result["pokemon_entries"]][20:24]
+    species_urls = [entry["pokemon_species"]["url"] for entry in result["pokemon_entries"]][45:47]
     #species_urls = [entry["pokemon_species"]["url"] for entry in result["pokemon_entries"]]
     return species_urls
 
@@ -40,14 +40,43 @@ async def get_varieties_data(varieties_url):
         resp = await client.get(pokemon_url)
         return resp.json()
 
-
 async def parse_varieties_urls(varieties_urls):
     tasks = [asyncio.create_task(get_varieties_data(varieties_url)) for varieties_url in varieties_urls]
-    varieties = []
-    for coro in asyncio.as_completed(tasks):
-        result = await coro
-        id = result["id"]
-        name = result["name"]
+    pokemon_entries = {}
+    #for coro in asyncio.as_completed(tasks):
+    for task in asyncio.as_completed(tasks):
+        result = await task
+
+        pokemon_name = result["name"]
+        pokemon_entry = {
+            "id": result["id"],
+            "types": [
+                {
+                    "slot": type_entry["slot"], 
+                    "name": type_entry["type"]["name"]
+                } 
+                    for type_entry in result["types"]
+            ],
+            "stats": [
+                {
+                    "name": stat_entry["stat"]["name"],
+                    "yield": stat_entry["effort"]
+                }
+                    for stat_entry in result["stats"]
+            ]
+        }
+
+        pokemon_entries[pokemon_name] = pokemon_entry
+        return pokemon_entries
+ 
+
+POKEDEX = 'https://pokeapi.co/api/v2/pokedex/32'
+species_urls = get_species_urls(POKEDEX)
+asyncio.run(get_varieties_from_datasets(species_urls))
+
+'''
+        id = results["id"]
+        name = results["name"]
 
         variety = {
             "id": id,
@@ -60,12 +89,6 @@ async def parse_varieties_urls(varieties_urls):
             type_name = type_entry["type"]["name"]
             type_list = {"slot": type_slot, "name": type_name}
             variety["types"].append(type_list)
-        #print(variety,type(variety))
-        varieties.append(variety)
-    print(varieties)
+        print(variety)
+'''
 
-
-
-POKEDEX = 'https://pokeapi.co/api/v2/pokedex/32'
-species_urls = get_species_urls(POKEDEX)
-asyncio.run(get_varieties_from_datasets(species_urls))
